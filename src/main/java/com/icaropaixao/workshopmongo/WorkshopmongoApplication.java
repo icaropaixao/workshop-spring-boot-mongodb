edb8a3f647214966f9e93a71b726f3575867cb61
Ícaro Reis <icaroreis1982@gmail.com>
2025-03-21 20:08:02 -0300

    Primeiro commit (Projeto criado)

---
+++ src/main/java/com/icaropaixao/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,16 @@
+package com.icaropaixao.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+
+
+		
+	}
+
+}
---
+++ src/test/java/com/icaropaixao/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icaropaixao.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
