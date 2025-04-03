c0203db54432e7d90c5c9241152ac9b1a6b4d714
Ícaro Reis <icaroreis1982@gmail.com>
2025-04-03 18:03:28 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/icaropaixao/workshopmongo/config/Instantietion.java
@@ -0,0 +1,34 @@
+package com.icaropaixao.workshopmongo.config;
+
+import com.icaropaixao.workshopmongo.domain.User;
+import com.icaropaixao.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration // Torna essa classe um Bean gerenciado pelo Spring
+public class Instantietion implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository; // Injeção de dependência do repositório
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll(); // deleta todos os usuarios e adiciona os solicitados
+        // Criando usuários
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        // Salvando no banco de dados
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        System.out.println("Usuários salvos no banco!");
+    }
+
+
+}
+
