a8a848eedf3635f7523872a1e21f5e6bfb48a96c
Ícaro Reis <icaroreis1982@gmail.com>
2025-07-07 19:20:50 -0300

    Criação do CommentDTO

--- src/main/java/com/icaropaixao/workshopmongo/domain/Post.java
+++ src/main/java/com/icaropaixao/workshopmongo/domain/Post.java
@@ -1,13 +1,14 @@
 package com.icaropaixao.workshopmongo.domain;
 
 import com.icaropaixao.workshopmongo.dto.AuthorDTO;
+import com.icaropaixao.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Objects;
-
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -15,31 +16,22 @@ public class Post implements Serializable {
 
     @Id
     private String id;
-
     private Date date;
     private String title;
     private String body;
     private AuthorDTO author;
 
-    //Constructoe
+    private List<CommentDTO> comments = new ArrayList<>();
+
     public Post() {
     }
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        super();
         this.id = id;
         this.date = date;
         this.title = title;
         this.body = body;
-        this.author = author;
-    }
-
-    //gets sets
-
-    public AuthorDTO getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
@@ -75,20 +67,45 @@ public class Post implements Serializable {
         this.body = body;
     }
 
+    public AuthorDTO getAuthor() {
+        return author;
+    }
 
-    //hascode
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Post post = (Post) o;
-        return Objects.equals(id, post.id);
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Post other = (Post) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
