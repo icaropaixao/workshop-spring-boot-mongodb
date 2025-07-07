237ad4c3018e25d5f3bec87e20fc8059fee1ce3a
Ícaro Reis <icaroreis1982@gmail.com>
2025-07-07 18:33:00 -0300

    Referenciando posts de usuarios

--- src/main/java/com/icaropaixao/workshopmongo/config/Instantietion.java
+++ src/main/java/com/icaropaixao/workshopmongo/config/Instantietion.java
@@ -44,10 +44,11 @@ public class Instantietion implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia!", "Acordei Feliz hoje!", new AuthorDTO(maria));
-
-
-        // Salvando no banco de dados
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
 
         System.out.println("Usuários e posts salvos no banco de dados!");
     }
--- src/main/java/com/icaropaixao/workshopmongo/domain/User.java
+++ src/main/java/com/icaropaixao/workshopmongo/domain/User.java
@@ -1,59 +1,90 @@
 package com.icaropaixao.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Document(collection = "user")
-public class User  implements Serializable {
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     private String id;
-
     private String name;
     private String email;
 
-    // constructors
-    public User(){
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User() {
     }
+
     public User(String id, String name, String email) {
+        super();
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
-    // gets and sets
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-    // hashCode and equals
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
+    public List<Post> getPosts() {
+        return posts;
     }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
